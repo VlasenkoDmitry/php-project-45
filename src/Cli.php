b8106198a9e6db11fbc9a1709f45fb4fa8097f36
DzmitryV <vlasenko.dmitry1@gmail.com>
2024-02-04 14:48:15 +0300

    style: fix errors according to linter recommendations

--- src/Cli.php
+++ src/Cli.php
@@ -5,11 +5,9 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcome ()
+function welcome()
 {
-	line('Welcome to the Brain Game!');
-	$name = prompt('May I have your name?');
-	line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
-
-
