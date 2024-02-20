3e5d6dc08c2c6d95430c2eca91ed46923495eb6f
DzmitryV <vlasenko.dmitry1@gmail.com>
2024-02-20 12:02:44 +0300

    fix: add text to welcome

--- src/Cli.php
+++ src/Cli.php
@@ -8,4 +8,7 @@ use function cli\prompt;
 function welcome()
 {
     line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, {$name}");
+    return $name;
 }
